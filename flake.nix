9bc478e07dd8dd6cb48ce95b698e95d0ff5c2d77
hugosenari <hugosenari@gmail.com>
2022-05-12 11:53:02 -0300

    chore: re index jestercookiemsgs

--- flake.nix
+++ flake.nix
@@ -7,6 +7,22 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."jestercookiemsgs-master".dir   = "master";
+  inputs."jestercookiemsgs-master".owner = "nim-nix-pkgs";
+  inputs."jestercookiemsgs-master".ref   = "master";
+  inputs."jestercookiemsgs-master".repo  = "jestercookiemsgs";
+  inputs."jestercookiemsgs-master".type  = "github";
+  inputs."jestercookiemsgs-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."jestercookiemsgs-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."jestercookiemsgs-v1_0_1".dir   = "v1_0_1";
+  inputs."jestercookiemsgs-v1_0_1".owner = "nim-nix-pkgs";
+  inputs."jestercookiemsgs-v1_0_1".ref   = "master";
+  inputs."jestercookiemsgs-v1_0_1".repo  = "jestercookiemsgs";
+  inputs."jestercookiemsgs-v1_0_1".type  = "github";
+  inputs."jestercookiemsgs-v1_0_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."jestercookiemsgs-v1_0_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
